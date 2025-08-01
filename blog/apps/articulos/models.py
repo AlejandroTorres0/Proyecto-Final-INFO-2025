2d79603931b424836ae84faa5b1f6148a2344e35
AlejandroTorres0 <at7709739@gmail.com>
2025-08-01 19:18:38 -0300

    Modelos creados y herencia de templates

--- blog/apps/articulos/models.py
+++ blog/apps/articulos/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from categorias.models import Categoria
 
 # Create your models here.
+class Articulo(models.Model):
+    #ATRIBUTOS del ARTICULO
+    creado = models.DateTimeField(auto_now_add=True)    
+    modificado = models.DateTimeField(auto_now=True)
+    titulo = models.CharField(max_length=150)
+    slug = models.SlugField(unique=True)
+    imagen = models.ImageField(upload_to = 'articulos')
+    contenido = models.TextField()
+    categoria = models.ForeignKey(Categoria, on_delete = models.CASCADE)
+    #categoria autor likes
+
+    def __str__(self):
+        return self.titulo
--- blog/apps/comentarios/models.py
+++ blog/apps/comentarios/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from usuarios.models import Usuario
+from articulos.models import Articulo
 
 # Create your models here.
+class Comentario(models.Model):
+
+    #ATRIBUTOS del COMENTARIO
+    usuario = models.ForeignKey(Usuario, on_delete= models.CASCADE)
+    articulo = models.ForeignKey(Articulo, on_delete=models.CASCADE)
+    contenido = models.CharField(max_length=250)
+    likes = models.IntegerField()
+    creado = models.DateTimeField(auto_now_add=True)    
+    modificado = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.texto #podria ser nombre de usuario y fecha nomas
