249b25ae738590f36c193e9ed09b504a24e40157
AlejandroTorres0 <at7709739@gmail.com>
2025-08-04 23:13:33 -0300

    Paginado basico de articulos

--- blog/apps/articulos/views.py
+++ blog/apps/articulos/views.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render
 from .models import Articulo
+from django.core.paginator import Paginator 
+
 # Create your views here.
 
 def Listar_Articulos(request):
     # Aquí iría la lógica para listar los artículos
-    MESES = {
-        1: "Enero", 2: "Febrero", 3: "Marzo", 4: "Abril",
-        5: "Mayo", 6: "Junio", 7: "Julio", 8: "Agosto",
-        9: "Septiembre", 10: "Octubre", 11: "Noviembre", 12: "Diciembre"
-    }
+    p = Paginator(Articulo.objects.all(), 1) #Cuantos articulos se van a mostrar por página
+    page = request.GET.get('page')
+    articulos_paginados = p.get_page(page)
 
     articulos_bd = Articulo.objects.all()
 
-    return render(request, 'articulos/blog.html', {'articulos':articulos_bd, 'meses': MESES})
+    total_paginas = p.num_pages
+    pagina_actual = articulos_paginados.number
+
+    tamano_bloque = 6
+    principio_bloque = ((pagina_actual - 1) // tamano_bloque) * tamano_bloque + 1
+    final_bloque = min(principio_bloque + tamano_bloque, total_paginas + 1)
+
+    rango_paginas = range(principio_bloque, final_bloque)
+
+    context = {
+        'articulos':articulos_bd, 
+        'articulos_p': articulos_paginados, 
+        'rango_paginas': rango_paginas
+    }
+
+    return render(request, 'articulos/blog.html', context)
 
