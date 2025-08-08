2014a2422ee9dd80f8a687ab5cb935d1cf67fa74
AlejandroTorres0 <at7709739@gmail.com>
2025-08-08 18:26:34 -0300

    Editar y Eliminar comentarios

--- blog/apps/comentarios/views.py
+++ blog/apps/comentarios/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, HttpResponseRedirect
 from .models import Comentario, Articulo, LikeComentario
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
-
+from django.views.generic.edit import UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin 
+from .forms import FormularioEditarComentario
 # Create your views here.
 @login_required
 def Comentar(request, pk):
@@ -13,6 +15,25 @@ def Comentar(request, pk):
 
     return HttpResponseRedirect(reverse_lazy('articulos:path_articulo_detalle', kwargs = {'pk': pk} ))
 
+
+class EditarComentario(UpdateView, LoginRequiredMixin): 
+    model = Comentario
+    form_class = FormularioEditarComentario
+    template_name = 'Comentarios/editar_comentario.html'
+
+    #Verificacion de que el comentario es del usuario que hace la request      
+
+    def get_success_url(self):
+        return reverse_lazy('articulos:path_articulo_detalle', kwargs={'pk':self.object.articulo.pk})
+
+class EliminarComentario(DeleteView, LoginRequiredMixin): 
+    model = Comentario
+    template_name = 'comentarios/eliminar_comentario.html'
+
+    #Verificacion de que el comentario es del usuario que hace la request      
+
+    def get_success_url(self):
+        return reverse_lazy('articulos:path_articulo_detalle', kwargs={'pk':self.object.articulo.pk})
 
 @login_required
 def LikearComentario(request, pk_comentario, pk_articulo):
