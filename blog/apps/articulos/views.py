94b8a51d250cf7a20c07e36d5ef64b74984b2e49
Agustinf18 <Agustinf18@outlook.es>
2025-08-07 22:30:41 -0300

    filtro articulos mi rama

--- blog/apps/articulos/views.py
+++ blog/apps/articulos/views.py
@@ -7,6 +7,7 @@ from .forms import ArticuloForm
 # Create your views here.
 
 def Listar_Articulos(request):
+    
     # Aquí iría la lógica para listar los artículos
     p = Paginator(Articulo.objects.all(), 1) #Cuantos articulos se van a mostrar por página
     page = request.GET.get('page')
@@ -43,3 +44,45 @@ def Crear_Articulo(request):
     else:
         form = ArticuloForm()
     return render(request, 'articulos/crear_articulo.html', {'form': form})
+
+
+def Filtrar_Categoria(request, pk):
+    categoria_filtrada = Categoria.objects.get(pk = pk)
+
+    articulos_filtrados = Articulo.objects.filter(categoria = categoria_filtrada)
+
+    valor_a_ordenar = request.GET.get('orden', None)
+    if valor_a_ordenar:
+        if valor_a_ordenar == 'asc':
+            articulos_ordenados = articulos_filtrados.order_by('creado')
+        else:
+            articulos_ordenados = articulos_filtrados.order_by('-creado')
+    else:
+        articulos_ordenados = articulos_filtrados
+    
+
+    categorias_bd = Categoria.objects.all()
+
+    p = Paginator(articulos_ordenados, 1) #Cuantos articulos se van a mostrar por página
+    page = request.GET.get('page')
+    articulos_paginados = p.get_page(page)
+
+    total_paginas = p.num_pages
+    pagina_actual = articulos_paginados.number
+
+    tamano_bloque = 6
+    principio_bloque = ((pagina_actual - 1) // tamano_bloque) * tamano_bloque + 1
+    final_bloque = min(principio_bloque + tamano_bloque, total_paginas + 1)
+
+    rango_paginas = range(principio_bloque, final_bloque)
+
+    context = {
+            'categorias': categorias_bd,
+            'articulos_p': articulos_paginados, 
+            'id_categoria': categoria_filtrada.id,
+            'nombre_categoria': categoria_filtrada.nombre,
+            'rango_paginas': rango_paginas
+    }
+
+    return render(request, 'articulos/blog.html', context)
+
