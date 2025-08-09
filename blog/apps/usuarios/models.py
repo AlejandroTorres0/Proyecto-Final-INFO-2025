82e1acd98eb513d53b4e4043215d91024eb67e32
AlejandroTorres0 <at7709739@gmail.com>
2025-08-08 23:54:26 -0300

    Creación de perfil con el registro

--- blog/apps/usuarios/models.py
+++ blog/apps/usuarios/models.py
@@ -1,11 +1,20 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
 
 # Create your models here.
 class PerfilUsuario(models.Model):
     usuario = models.OneToOneField(User, on_delete=models.CASCADE, related_name='perfil')
-    avatar = models.ImageField(upload_to = 'usuario/')
+    avatar = models.ImageField(upload_to = 'usuario/', default='usuario/default.png')
 
 
     def __str__(self):
         return self.usuario.username
+    
+
+def crearPerfilUsuario(sender, instance, created, **kwargs):
+    if created: 
+        perfil_usuario = PerfilUsuario(usuario=instance)
+        perfil_usuario.save()
+
+post_save.connect(crearPerfilUsuario, sender = User)
